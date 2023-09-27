6b29425daa0bdbd1e2a94bc720f3221937ef27d1
Igor Chvalun <igor.chvalun@investengine.com>
2023-09-27 15:56:26 +0530

    Mine is better

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -2,6 +2,11 @@ package school.mjc.stage0.loops.task4;
 
 public class NumberToBreakOn {
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
-
+        if(toBreakWith >= numberToGoUntil) {
+            System.out.println("iterating till the end");
+        }
+        for (int i = 1; i <= Math.min(toBreakWith, numberToGoUntil); i++) {
+            System.out.println(i);
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
+++ src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
@@ -2,6 +2,19 @@ package school.mjc.stage0.loops.task4;
 
 public class SkippingThird {
     public void printUntilButThird(int lastPrinted) {
-
+        int counter = 0;
+        for (int i = 1; i <= lastPrinted; i++) {
+            if(i%3 != 0) {
+                System.out.println(i);
+            }
+            /*
+            if(counter == 2) {
+                continue;
+            } else {
+                System.out.println(i);
+            }
+            counter++;
+             */
+        }
     }
 }
